baeed46549d95581acc183b8dd20e6dbd4fdf9bb
TranDavid1 <dtrancpp@gmail.com>
2023-04-27 05:01:22 -0700

    add hover effect and highlighting to navbar icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 import { IconButton } from "@mui/material";
@@ -7,32 +7,50 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import InventoryIcon from "@mui/icons-material/Inventory";
 
 function Navbar() {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleClick = (index) => {
+        setActiveIndex(index);
+    };
+
     return (
         <nav className="Navbar">
             <ul>
-                <li>
+                <li
+                    className={activeIndex === 0 ? "active" : ""}
+                    onClick={() => handleClick(0)}
+                >
                     <IconButton
                         component={Link}
                         to="/items"
                         aria-label="Inventory"
+                        className="MuiIconButton-root"
                     >
                         <HomeIcon />
                     </IconButton>
                 </li>
-                <li>
+                <li
+                    className={activeIndex === 1 ? "active" : ""}
+                    onClick={() => handleClick(1)}
+                >
                     <IconButton
                         component={Link}
                         to="/dashboard"
                         aria-label="Dashboard"
+                        className="MuiIconButton-root"
                     >
                         <DashboardIcon />
                     </IconButton>
                 </li>
-                <li>
+                <li
+                    className={activeIndex === 2 ? "active" : ""}
+                    onClick={() => handleClick(2)}
+                >
                     <IconButton
                         component={Link}
                         to="/items"
                         aria-label="Inventory"
+                        className="MuiIconButton-root"
                     >
                         <InventoryIcon />
                     </IconButton>
