f181a190ea3cdb48a3c8b2a42d7348eecf38005b
TranDavid1 <dtrancpp@gmail.com>
2023-04-26 19:40:19 -0700

    create add item form that appears on button click

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import InventoryList from "./components/InventoryList";
 
 function App() {
     const [inventory, setInventory] = useState([]);
+    const [showForm, setShowForm] = useState(false);
 
     const handleAddItem = (newItem) => {
         setInventory([...inventory, newItem]);
+        setShowForm(false);
+    };
+
+    const handleShowForm = () => {
+        setShowForm(true);
     };
 
     return (
         <div className="App">
             <h1>Inventory Management App</h1>
-            <InventoryForm onAddItem={handleAddItem} />
+            {!showForm && <button onClick={handleShowForm}>Add Item</button>}
+            {showForm && <InventoryForm onAddItem={handleAddItem} />}
             <InventoryList inventory={inventory} />
         </div>
     );
--- src/components/InventoryForm.js
+++ src/components/InventoryForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-function InventoryForm() {
+function InventoryForm(props) {
     const [itemName, setItemName] = useState("");
     const [itemDescription, setItemDescription] = useState("");
     const [itemQuantity, setItemQuantity] = useState(0);
+    const [itemPhoto, setItemPhoto] = useState(null);
 
     const handleNameChange = (event) => {
         setItemName(event.target.value);
@@ -17,14 +18,71 @@ function InventoryForm() {
         setItemQuantity(event.target.value);
     };
 
+    const handlePhotoChange = (event) => {
+        setItemPhoto(event.target.files[0]);
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(
-            `Submitting item ${itemName} with description ${itemDescription} and quantity ${itemQuantity}`
-        );
+
+        const newItem = {
+            id: Date.now(),
+            name: itemName,
+            description: itemDescription,
+            quantity: itemQuantity,
+            photo: itemPhoto,
+        };
+
+        props.onAddItem(newItem);
+        // console.log(
+        //     `Submitting item ${itemName} with description ${itemDescription} and quantity ${itemQuantity}`
+        // );
         setItemName("");
         setItemDescription("");
+        setItemQuantity(0);
+        setItemPhoto(null);
     };
+
+    return (
+        <div className="InventoryForm">
+            <h2>Add New Item</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={itemName}
+                    onChange={handleNameChange}
+                    required
+                />
+                <label htmlFor="description">Description:</label>
+                <input
+                    type="description"
+                    id="description"
+                    value={itemDescription}
+                    onChange={handleDescriptionChange}
+                    required
+                />
+                <label htmlFor="quantity">Quantity:</label>
+                <input
+                    type="number"
+                    id="quantity"
+                    value={itemQuantity}
+                    onChange={handleQuantityChange}
+                    required
+                />
+                <label htmlFor="photo">Photo:</label>
+                <input
+                    type="file"
+                    id="photo"
+                    accept="image/*"
+                    onChange={handlePhotoChange}
+                    required
+                />
+                <button type="submit">Add Item</button>
+            </form>
+        </div>
+    );
 }
 
 export default InventoryForm;
