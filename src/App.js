885c15ee710b030683e1e7537f9bf0b0d6e9ba26
TranDavid1 <dtrancpp@gmail.com>
2023-04-26 18:50:55 -0700

    add filter, search, delete, and edit items, create InventoryForm.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import InventoryForm from "./components/InventoryForm";
+import InventoryList from "./components/InventoryList";
 
 function App() {
+    const [inventory, setInventory] = useState([]);
+
+    const handleAddItem = (newItem) => {
+        setInventory([...inventory, newItem]);
+    };
+
     return (
         <div className="App">
             <h1>Inventory Management App</h1>
-            <InventoryForm />
+            <InventoryForm onAddItem={handleAddItem} />
+            <InventoryList inventory={inventory} />
         </div>
     );
 }
--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,17 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import InventoryItem from "./InventoryItem";
 
 function InventoryList(props) {
-    const { inventory } = props.inventory;
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [filterValue, setFilterValue] = useState("");
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleSortOrder = () => {
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    };
+
+    const handleFilterValue = (event) => {
+        setFilterValue(event.target.value);
+    };
+
+    const handleSearchValue = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    const handleDeleteItem = (id) => {
+        const updatedInventory = props.inventory.filter(
+            (item) => item.id !== id
+        );
+        props.onDeleteItem(updatedInventory);
+    };
+
+    const handleEditItem = (id, updatedItem) => {
+        const updatedInventory = props.inventory.map((item) =>
+            item.id === id ? updatedItem : item
+        );
+        props.onEditItem(updatedInventory);
+    };
+
+    const sortedInventory = [...props.inventory].sort((a, b) => {
+        if (sortOrder === "asc") {
+            return a.name.localeCompare(b.name);
+        } else {
+            return b.name.localeCompare(a.name);
+        }
+    });
+
+    const filteredInventory = sortedInventory.filter((item) => {
+        return item.name.toLowerCase().includes(filterValue.toLowerCase());
+    });
+
+    const searchedInventory = filteredInventory.filter((item) => {
+        return item.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
 
     return (
         <div className="InventoryList">
             <h2>Inventory List</h2>
-            <ul>
-                {inventory.map((item) => {
-                    <InventoryItem key={item.id} item={item} />;
-                })}
-            </ul>
+            <div>
+                <label htmlFor="sortOrder">Sort by Name:</label>
+                <button onClick={handleSortOrder}>
+                    {sortOrder === "asc" ? "Ascending" : "Descending"}
+                </button>
+            </div>
+            <div>
+                <label htmlFor="filterValue">Filter by Name:</label>
+                <input
+                    type="text"
+                    id="filterValue"
+                    value={filterValue}
+                    onChange={handleFilterValue}
+                />
+            </div>
+            <div>
+                <label htmlFor="searchValue">Search:</label>
+                <input
+                    type="text"
+                    id="searchValue"
+                    value={searchValue}
+                    onChange={handleSearchValue}
+                />
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchedInventory.length === 0 ? (
+                        <tr>
+                            <td colSpan="4">No items in inventory</td>
+                        </tr>
+                    ) : (
+                        searchedInventory.map((item) => (
+                            <InventoryItem
+                                key={item.id}
+                                item={item}
+                                onDeleteItem={handleDeleteItem}
+                                onEditItem={handleEditItem}
+                            />
+                        ))
+                    )}
+                </tbody>
+            </table>
         </div>
     );
 }
