b6448acf33ce8042780a9eba58876f1f8fd5f512
TranDavid1 <dtrancpp@gmail.com>
2023-04-29 05:20:45 -0700

    start on edit item components

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../css/Item.css";
-import { Link, useParams } from "react-router-dom";
+import { Form, Link, useParams } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 function Item() {
     const { itemId } = useParams();
@@ -22,10 +24,40 @@ function Item() {
     return (
         <div className="item-container">
             {item ? (
-                <h2 className="item-header">{item.itemName}</h2>
+                <>
+                    <div className="item-header">
+                        <h2 className="item-name">{item.itemName}</h2>
+                        <div className="save-button-container">
+                            <Button className="save-button">Save</Button>
+                        </div>
+                    </div>
+                    <div className="item-info">
+                        <div className="item-id">Item ID: {item.id}</div>
+                        <div className="item-quantity">
+                            Quantity: {item.itemQuantity} units
+                        </div>
+                    </div>
+                    <div className="item-edit-container">
+                        <div className="edit-quantity-container">
+                            <TextField
+                                className="edit-quantity-text-field"
+                                variant="standard"
+                                id="edit-quantity-text-field"
+                                value={item.itemQuantity}
+                                label="Quantity"
+                            />
+                        </div>
+                    </div>
+                </>
             ) : (
                 <p> Loading item...</p>
             )}
+            {/* <div className="item-info">
+                <div className="item-id">Item ID: {item.id}</div>
+                <div className="item-quantity">
+                    Quantity: {item.itemQuantity} units
+                </div>
+            </div> */}
         </div>
     );
 }
