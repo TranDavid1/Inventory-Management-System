d607fc13f9ec182c5c107fd26cd35c78900cf85a
TranDavid1 <dtrancpp@gmail.com>
2023-12-07 22:01:27 -0800

    start on simple authController for authentication

---
+++ node-postgres/controllers/authController.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+
+exports.login = (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({ message: info.message });
+        }
+
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            return res.status(200).json({ message: "Login successful!", user });
+        });
+    })(req, res, next);
+};
+
+exports.logout = (req, res) => {
+    req.logout();
+    res.status(200).json({ message: "Logout successful!" });
+};
